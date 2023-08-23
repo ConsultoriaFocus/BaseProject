f1b3d1cbcbb069ce0d9e24344f1523928791da5f
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2023-08-23 20:34:15 -0300

    alterando arquivo de configuração de testes

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    clearMocks: true,
+    collectCoverageFrom: ['src/endpoints/**/*.ts', '!src/**/*.entity.ts', '!src/**/*.repository.ts', '!src/**/index.ts'],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    setupFiles: ['<rootDir>/jest/setEnvVars.ts']
+};
