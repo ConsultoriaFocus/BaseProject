8ff698813229e123bcd9d47341a88144cf728723
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2023-08-23 21:16:59 -0300

    removendo dependências de teste e pré-commit

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    clearMocks: true,
-    collectCoverageFrom: ['src/endpoints/**/*.ts', '!src/**/*.entity.ts', '!src/**/*.repository.ts', '!src/**/index.ts'],
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    setupFiles: ['<rootDir>/jest/setEnvVars.ts']
-};
