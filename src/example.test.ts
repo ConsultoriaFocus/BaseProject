ce8c76aa0b0e20b61c1a58c17c9f789ead71f8f6
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2023-08-23 21:45:55 -0300

    test: implementando um teste de exemplo

---
+++ src/example.test.ts
@@ -0,0 +1,11 @@
+// Descrição mais geral do seu teste ex: CRUD user
+describe('Make a Test Example', () => {
+    // Descrição mais específica abordagem qual será o enfoque ex: POST user
+    describe('Testing boolean type', () => {
+        // Descreve qual questão você está verificando ex: Should returns 400 if email is invalid
+        test('Should a true variable equals true', () => {
+            const exampleVariable = true;
+            expect(exampleVariable).toEqual(true);
+        });
+    });
+});
